bd769242179f62f67f039861c983e4a2845cae05
Sylvain <sylvain.grison@sncf.fr>
2017-01-11 12:58:48 +0100

    modification de la page d'identification

--- application/views/identification.php
+++ application/views/identification.php
@@ -25,11 +25,6 @@
         <p>Mot de passe</p>
         <label for="inputPassword" class="sr-only">Password</label>
         <input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required>
-        <div class="checkbox">
-          <label>
-            <input type="checkbox" value="remember-me"> Remember me
-          </label>
-        </div>
         <button class="btn btn-lg btn-primary btn-block" type="submit">Identification</button>
       </form>
 
