b94e07aa0b16c69dac1ad7d805c2f3984a6de385
Grizoune <grisonsylvain@gmail.com>
2017-01-13 17:06:01 +0100

    ajout d'un traceur dans la cron

--- application/controllers/Serveur.php
+++ application/controllers/Serveur.php
@@ -5,14 +5,18 @@ class Serveur extends CI_Controller{
 
 	public function cron(){
 		$this->load->model('scheduler_model');
+		$this->load->library('Arduihome_log');
+		$this->load->library('Arduihome_demon');
+		$this->arduihome_log->write("infos", "execution de la cron");
+
 		$this->scheduler_model->execExpiredTask();
 		
-		$this->load->library('Arduihome_demon');
+		
 		if($this->arduihome_demon->getStatut() ==0){
 
 				$this->arduihome_demon->startProcess();
 				
-				$this->load->library('Arduihome_log');
+				
 				$this->arduihome_log->write("infos", "redemarrage automatique du serveur via la tache cron");
 		}
 
