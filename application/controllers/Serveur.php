50711d540b715f7214a19af1588e5e0c003e1ed3
Grizoune <grisonsylvain@gmail.com>
2017-01-13 17:58:10 +0100

    correctif

--- application/controllers/Serveur.php
+++ application/controllers/Serveur.php
@@ -24,6 +24,6 @@ class Serveur extends CI_Controller{
 	public function start()
 	{
 		$this->load->library('Arduihome_demon');
-		$this->arduihome_demon->startProcess();
+		$this->arduihome_demon->build_demon();
 	}
 }
