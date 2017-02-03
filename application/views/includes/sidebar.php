9de2589afc120f99320298e8af6d4c763366b3d7
Grizoune <grisonsylvain@gmail.com>
2017-02-03 22:26:56 +0100

    correctifs cosmetic

--- application/views/includes/sidebar.php
+++ application/views/includes/sidebar.php
@@ -1,8 +1,8 @@
 		 <div class="col-md-2 sidebar">
 		          <ul>
-		            <li class="active"><a href="<?php echo site_url('dashboard/index'); ?>"><i class="icon-fav"></i> Favoris</a></li>
+		            <li class="<?php if(!$zone->id)echo "active"; ?>"><a href="<?php echo site_url('dashboard/index'); ?>"><i class="icon-fav"></i> Favoris</a></li>
 		            <?php foreach($this->zone_model->getZones() as $zone_sidebar){ ?>
-		            	<li><nobr><a href="<?php echo site_url('dashboard/index/'.$zone_sidebar->id); ?>"><i class="icon" style="background-image: url('<?php echo base_url().'assets/img/icons/'.$zone_sidebar->image.'.svg'; ?>');"></i> <?php echo $zone_sidebar->zone; ?></a></nobr></li>
+		            	<li class="<?php if($zone->id==$zone_sidebar->id)echo "active"; ?>"><nobr><a href="<?php echo site_url('dashboard/index/'.$zone_sidebar->id); ?>"><i class="icon" style="background-image: url('<?php echo base_url().'assets/img/icons/'.$zone_sidebar->image.'.svg'; ?>');"></i> <?php echo $zone_sidebar->zone; ?></a></nobr></li>
 		            <?php } ?>
 		          </ul>
 		 </div>
