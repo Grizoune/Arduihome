c4aef43e35364e2d9c3c418a428a5e385efa069d
Sylvain <grisonsylvain@gmail.com>
2019-03-31 21:58:57 +0200

    ajout du widget temperature

---
+++ application/views/widget/temperature.php
@@ -0,0 +1,4 @@
+
+<span style="font-size:30pt;padding: 5px;"><?php echo $periph->valeur; ?>°C</span>
+
+<span style="font-size: 8pt;"><i>Dernier relevé : <?php echo $periph->last_heartbeat; ?></i></span>
